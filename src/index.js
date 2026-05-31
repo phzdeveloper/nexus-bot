d44a8d968a74e2d94b6f3a27a464af6674881b8d
phzdeveloper <pedrohenriqueaguiarmaciel@gmail.com>
2026-05-31 06:25:01 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
+require("dotenv").config();
 
+const fs = require("fs");
+const path = require("path");
+
+const {
+    Client,
+    GatewayIntentBits,
+    Collection
+} = require("discord.js");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers
+    ]
+});
+
+client.commands = new Collection();
+
+const commandFolders = fs.readdirSync(
+    path.join(__dirname, "commands")
+);
+
+for (const folder of commandFolders) {
+
+    const commandFiles = fs
+        .readdirSync(path.join(__dirname, "commands", folder))
+        .filter(file => file.endsWith(".js"));
+
+    for (const file of commandFiles) {
+
+        const command = require(
+            path.join(__dirname, "commands", folder, file)
+        );
+
+        client.commands.set(
+            command.data.name,
+            command
+        );
+    }
+}
+
+const eventFiles = fs
+    .readdirSync(path.join(__dirname, "events"))
+    .filter(file => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+
+    const event = require(
+        path.join(__dirname, "events", file)
+    );
+
+    if (event.once) {
+        client.once(event.name, (...args) =>
+            event.execute(...args, client)
+        );
+    } else {
+        client.on(event.name, (...args) =>
+            event.execute(...args, client)
+        );
+    }
+}
+
+client.login(process.env.TOKEN);
