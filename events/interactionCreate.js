048ac4f774c70a725e3bcda44f32c1b024d51c1f
phzdeveloper <pedrohenriqueaguiarmaciel@gmail.com>
2026-05-31 06:25:40 -0300

    Update and rename events to interactionCreate.js

---
+++ events/interactionCreate.js
@@ -0,0 +1,13 @@
+const { Events } = require("discord.js");
+
+module.exports = {
+    name: Events.ClientReady,
+    once: true,
+
+    execute(client) {
+
+        console.log(
+            `✅ ${client.user.tag} online.`
+        );
+    }
+};
